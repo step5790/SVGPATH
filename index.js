938c3c9dd9b38179fdf4439505bd251267187560
Stephen <stephen.s.espinosa@gmail.com>
2021-11-03 17:48:37 +0100

    initial

---
+++ index.js
@@ -0,0 +1,5 @@
+import "/style.scss";
+
+import test from "/assets/test.png";
+
+document.querySelector("img").src = test;
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+
+module.exports = defineConfig({
+  base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
+});
